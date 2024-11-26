27e0403d599ef53152672e8aedc15f3b45854c33
김리희 <sksgml6530@gmail.com>
2024-11-26 15:05:52 +0900

    수정했다

---
+++ js/story_dessert.js
@@ -0,0 +1,22 @@
+window.addEventListener("load",function(){
+    
+    const station5 =new Swiper(".station6",{
+        slidesPerView: 1,
+        spaceBetween: 14,
+        breakpoints:{
+            768:{
+                slidesPerView: 2,
+                spaceBetween: 18,
+            },
+            1280:{
+                slidesPerView: 4,
+                spaceBetween: 20,
+            },
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    })
+
+})
